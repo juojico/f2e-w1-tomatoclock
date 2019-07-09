d654f67005b8e407a215d61fe445cac39ab9fea3
juojico <u4m062@gmail.com>
2019-07-10 02:26:24 +0800

    merge

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Tomato className="App-logo" />
+        <Tomato />
         <Timer />
       </header>
     </div>
