cf10ec480c66017685a8fe2e8118b4fbdbff3ef7
juojico <u4m062@gmail.com>
2019-07-10 22:39:22 +0800

    timer

--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -7,25 +7,52 @@ const TimerWrapper = styled.h1`
   color: #f05550;
 `;
 
+const Button = styled.button`
+  position: relative;
+  width: 300px;
+  background-color: #f05550;
+  border: 2px solid #e04540;
+  border-radius: 1em;
+  color: white;
+  font-size: 1em;
+  padding: .25em;
+  margin-top: .5em;
+  cursor: pointer;
+  :hover {
+    background-color: #ff6560;
+  }
+`;
+
 const minutes = seconds => Math.floor(seconds / 60) + ' : ' + (seconds % 60 < 10 ? '0' + seconds % 60 : seconds % 60);
 
 class Timer extends React.PureComponent {
   constructor(props) {
     super(props);
     this.state = {
-      seconds: 1500
+      seconds: 1500,
+      intervalId: '',
+      play: false
     };
   }
 
-  start = () => {
-    setInterval(() => this.setState({ seconds: this.state.seconds - 1 }), 1000)
+  start = () => this.setState({ seconds: this.state.seconds - 1 })
+
+  componentDidMount() {
+    let intervalId = setInterval(this.start, 1000)
+    this.setState({ intervalId: intervalId, play: false })
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId)
+    this.setState({ play: true })
   }
 
   render() {
     return (
       <TimerWrapper>
-        <button onClick={this.start} >start</button>
-        TOMATO {minutes(this.state.seconds)}
+        {minutes(this.state.seconds)}
+        <Button onClick={() => this.componentDidMount()} hidden={this.state.play ? false : true} >start</Button>
+        <Button onClick={() => this.componentWillUnmount()} hidden={this.state.play ? true : false} >stop</Button>
       </TimerWrapper>
     );
   }
