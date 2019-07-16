9c05f7e5a2e5b69bd3a91237c5901fc2efde37f7
jico <u4m062@gmail.com>
2019-07-16 22:50:00 +0800

    Updates

--- precache-manifest.9f46752efcf3fe77295ee585b1cced69.js
+++ precache-manifest.9f46752efcf3fe77295ee585b1cced69.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "02d4c145bc023bc314163bb30dadf9c2",
+    "revision": "db60b949e2c546caa07ac50ae954ff55",
     "url": "/f2e-w1-tomatoclock/index.html"
   },
   {
-    "revision": "d43f2da67bd33c53fca9",
+    "revision": "5cffbf52b83520fdd841",
     "url": "/f2e-w1-tomatoclock/static/css/main.34de6062.chunk.css"
   },
   {
-    "revision": "e7970407306a67abc9f8",
-    "url": "/f2e-w1-tomatoclock/static/js/2.8016225f.chunk.js"
+    "revision": "f20c024509d74b504d10",
+    "url": "/f2e-w1-tomatoclock/static/js/2.d9b2c027.chunk.js"
   },
   {
-    "revision": "d43f2da67bd33c53fca9",
-    "url": "/f2e-w1-tomatoclock/static/js/main.53105e56.chunk.js"
+    "revision": "5cffbf52b83520fdd841",
+    "url": "/f2e-w1-tomatoclock/static/js/main.b55dea98.chunk.js"
   },
   {
     "revision": "087080383a1f479fc96e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/f2e-w1-tomatoclock/precache-manifest.dccc5f9d526296fa9c4e6bdd32ceaa94.js"
+  "/f2e-w1-tomatoclock/precache-manifest.9f46752efcf3fe77295ee585b1cced69.js"
 );
 
 self.addEventListener('message', (event) => {
